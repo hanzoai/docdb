b15bf5382da825d1227178778162f02b4c4e6573
Zach Kelling <z@zeekay.io>
2026-06-10 18:50:23 -0700

    deps: prometheus/client_golang → luxfi/metric
    Mechanical migration following the beego v2.3.8 fork precedent
    (bc7b075). Same exposition surface, zero prometheus/client_golang
    in the dep graph.

--- internal/util/state/metrics_test.go
+++ internal/util/state/metrics_test.go
@@ -19,7 +19,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/prometheus/client_golang/prometheus/testutil"
+	metric "github.com/luxfi/metric"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
